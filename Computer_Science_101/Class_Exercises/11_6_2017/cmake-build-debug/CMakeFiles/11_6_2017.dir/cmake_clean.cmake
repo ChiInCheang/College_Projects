206a545a7b82ecde4b398880a12572582c8bf2b2
JaMES <jeandsmith27@gmail.com>
2017-11-07 09:56:48 -0500

    Added class project

---
+++ Computer_Science_101/Class_Exercises/11_6_2017/cmake-build-debug/CMakeFiles/11_6_2017.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11_6_2017.dir/main.cpp.obj"
+  "11_6_2017.pdb"
+  "11_6_2017.exe"
+  "11_6_2017.exe.manifest"
+  "lib11_6_2017.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_6_2017.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Computer_Science_101/Class_Exercises/11_6_2017/main.cpp
+++ Computer_Science_101/Class_Exercises/11_6_2017/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	double get_height();
+	double get_width();
+	double get_area(double, double);
+
+	cout << "Area of rect: " << get_area(get_width(), get_height()) << endl;
+
 	return 0;
+}
+
+double get_height() {
+	double height;
+	cout << "Enter height of rect: ";
+	cin >> height;
+	return height;
+}
+
+double get_width() {
+	double width;
+	cout << "Enter width of rect ";
+	cin >> width;
+	return width;
+}
+
+double get_area(double width, double height) {
+	return height * width;
 }
