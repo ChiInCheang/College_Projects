7eeff3af308cf17991df1ec92eb4d92f46e22cb9
James Smith Abreu <jeandsmith27@gmail.com>
2017-11-28 08:49:04 -0500

    Completed assignment

--- Computer_Science_101/Assignment_7/source-files/2.cpp
+++ Computer_Science_101/Assignment_7/source-files/2.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 void coin_toss() {
-        unsigned seed = static_cast<unsigned>(time(nullptr));
-        unsigned toss = rand() % 2 + 1;
-        if (toss == 1) {
+        random_device rdev;
+        uniform_int_distribution<int>dist(1,2);
+        if (dist(rdev) == 1) {
                 cout << "Head" << endl;
-        } else {
+        } else if (dist(rdev) == 2){
                 cout << "Tails" << endl;
         }
 }
@@ -19,17 +19,21 @@ int main() {
         cout << "Enter the number of times the coin will be tossed: ";
         int toss = 0;
         cin >> toss;
+
+//        While the user does not input a number greater than or 0
         while (toss < -1) {
                 cout << "Invalid input.\r" << endl;
                 cin >> toss;
         }
 
+//        If the user does not input any toss count
         if (toss == 0) {
                 cout << "The coin was not tossed." << endl;
                 return 0;
         }
 
-        int counter = 0;
+//        Toss the coin.
+        int counter = 1;
         while (counter <= toss) {
                 coin_toss();
                 counter++;
